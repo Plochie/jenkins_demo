165bf3fff26b8abf8b9a5512c70b30a813baf962
plochie <pareshlomate@gmail.com>
2020-06-07 18:23:54 +0530

    test

--- src/main/java/com/plochie/demo/DemoApplication.java
+++ src/main/java/com/plochie/demo/DemoApplication.java
@@ -15,7 +15,6 @@ public class DemoApplication implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("Project started and ended successfully.");
-		System.out.println();
 	}
 
 }
